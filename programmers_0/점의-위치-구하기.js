d77d1b073b942f84abef096d8afc5dc064b4b0db
heejiyang <dianoheeji@naver.com>
2022-11-09 01:48:24 +0900

    add: level 0

---
+++ programmers_0/점의-위치-구하기.js
@@ -0,0 +1,7 @@
+function solution(dot) {
+    if (dot[0] >= 0 && dot[1] >= 0) answer = 1;
+    if (dot[0] < 0 && dot[1] >= 0) answer = 2;
+    if (dot[0] < 0 && dot[1] < 0) answer = 3;
+    if (dot[0] >= 0 && dot[1] < 0) answer = 4;
+    return answer;
+}
